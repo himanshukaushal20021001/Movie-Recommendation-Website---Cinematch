17e3630a024191ae0bff146cc820fb87593bb5f8
himanshukaushal20021001 <harshkaushal20021001@gmail.com>
2025-08-25 14:57:50 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+const {nextui} = require('@nextui-org/theme');
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/[object Object].js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+      },
+    },
+  },
+  plugins: [nextui()],
+};
